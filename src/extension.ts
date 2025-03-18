eb46b8f34d0bd0403d23151895dad3305018d4e1
DasBossGit <130844666+DasBossGit@users.noreply.github.com>
2025-03-18 21:40:58 +0000

    🔧 Update logger variable declaration from const to let

--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ const outputChannel = vscode.window.createOutputChannel(EXTENSION_NAME, {
 });
 
 // 3. Create the Winston logger giving it the Log Output Channel
-export const logger = winston.createLogger({
+export let logger = winston.createLogger({
 	level: 'trace', // Recommended: set the highest possible level
 	levels: windowsTransportVscode.LogOutputChannelTransport.config.levels, // Recommended: use predefined VS Code log levels
 	format: windowsTransportVscode.LogOutputChannelTransport.format(), // Recommended: use predefined format
