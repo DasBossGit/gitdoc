84156dee8f8b44bbe79a1d4847125ec36d642810
DasBossGit <130844666+DasBossGit@users.noreply.github.com>
2025-03-31 01:41:49 +0000

    31.03.2025 03:41:49.839 `´AUTOCOMMIT`´

--- .vscode/main.js
+++ .vscode/main.js
@@ -1,33 +1,45 @@
 
 /// <reference path="c:/Users/Administrator/.vscode-insiders/extensions/nur.script-0.2.1/@types/api.global.d.ts" />
 /// <reference path="c:/Users/Administrator/.vscode-insiders/extensions/nur.script-0.2.1/@types/vscode.global.d.ts" />
-/// <reference path="c:/Users/Administrator/.vscode-insiders/extensions/nur.script-0.2.1/@types/vscode.global.d.ts" />
+/// <reference path="A:/Programming/JavaScript/gitdoc/node_modules/.pnpm/@types+minimatch@3.0.5/node_modules/@types/minimatch/index.d.ts" />
+
+const { match } = require("minimatch");
+/* const { matchesGlobPath } = require("path");
+const { matchesGlobPosix } = require("posix");
+const { matchesGlobWin } = require("win32");
+ */
 //  @ts-check
 //  API: https://code.visualstudio.com/api/references/vscode-api
 
 function activate(_context) {
-   output("clear");
+   /*    output("clear");
 
-   const git = extensions.getExtension("vscode.git").exports.getAPI(1);
+      const git = extensions.getExtension("vscode.git").exports.getAPI(1);
 
-   const repo = git.repositories[0];
+      const repo = git.repositories[0];
 
-   println(repo.state.workingTreeChanges);
-   println(repo.state.mergeChanges);
-   println(repo.state.indexChanges);
+      println(repo.state.workingTreeChanges);
+      println(repo.state.mergeChanges);
+      println(repo.state.indexChanges);
 
-   const changes = [
-      ...repo.state.workingTreeChanges,
-      ...repo.state.mergeChanges,
-      ...repo.state.indexChanges,
-   ];
+      const changes = [
+         ...repo.state.workingTreeChanges,
+         ...repo.state.mergeChanges,
+         ...repo.state.indexChanges,
+      ];
 
-   println(changes)
+      println(changes)
 
-   const changedUris = changes
-      .map((change) => change.uri);
+      const changedUris = changes
+         .map((change) => change.uri);
 
-   println(changedUris)
+      println(changedUris) */
+   let hay = "a:\\Programming\\JavaScript\\Proxmox_VM_Viewer\\bla.testfile";
+   let needle = ".testfile"
+   println(match(hay, needle))
+   println(matchesGlobPath(hay, needle))
+   println(matchesGlobPosix(hay, needle))
+   println(matchesGlobWin(hay, needle))
 }
 
 function deactivate() { }
