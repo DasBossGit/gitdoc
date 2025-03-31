e5dcf6239b3555653523a4bd6eedc14abf22c46d
DasBossGit <130844666+DasBossGit@users.noreply.github.com>
2025-03-31 02:36:32 +0000

    31.03.2025 04:36:32.120 `´AUTOCOMMIT`´

--- src/watcher.ts
+++ src/watcher.ts
@@ -443,7 +443,7 @@ export function watchForChanges(git: GitAPI): vscode.Disposable {
 
 	disposables.push(
 		vscode.window.onDidChangeActiveTextEditor((editor) => {
-			if (editor && matches(editor.document.uri, config.filePattern)) {
+			if (editor && (matches(editor.document.uri, config.filePattern) || !matches(editor.document.uri, config.excludeFilters, config.excludeFiltersCaseSense))) {
 				statusBarItem?.show();
 			} else {
 				statusBarItem?.hide();
@@ -453,7 +453,7 @@ export function watchForChanges(git: GitAPI): vscode.Disposable {
 
 	if (
 		vscode.window.activeTextEditor &&
-		matches(vscode.window.activeTextEditor.document.uri, config.filePattern)
+		(matches(vscode.window.activeTextEditor.document.uri, config.filePattern) || !matches(vscode.window.activeTextEditor.document.uri, config.excludeFilters, config.excludeFiltersCaseSense))
 	) {
 		statusBarItem?.show();
 	} else {
