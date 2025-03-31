308d3a8f3ea6e57571a63ed92ef6caccca81fe45
DasBossGit <130844666+DasBossGit@users.noreply.github.com>
2025-03-31 03:18:41 +0000

    31.03.2025 05:18:41.049 `´AUTOCOMMIT`´

--- src/extension.ts
+++ src/extension.ts
@@ -9,8 +9,6 @@ import { updateContext } from "./utils";
 import { logger } from "./logger";
 
 export async function activate(context: vscode.ExtensionContext) {
-	console.error(vscode.workspace.asRelativePath(vscode.Uri.parse("a:\\Programming\\JavaScript\\Proxmox_VM_Viewer\\bla.testfile"), true))
-
 	logger.info("Activating...");
 
 	const git = await getGitApi();
